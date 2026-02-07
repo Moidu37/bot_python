65331b36e304980f82ea9806be6e0c58081bfb9b
Moidu37 <juleshoudayer12@gmail.com>
2026-02-07 12:42:51 +0100

    Update bot_discord.py

--- bot_discord.py
+++ bot_discord.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from discord.ext import commands
+import requests
 
 # Récupère le token depuis une variable d'environnement
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -21,8 +22,23 @@ async def ping(ctx):
     await ctx.send("Pong 🏓")
 
 @bot.command()
-async def salut(ctx):
+async def helllo(ctx):
     await ctx.send("Salut, je suis ton bot hébergé sur PythonAnywhere !")
+
+@bot.command()
+async def meteo(ctx):
+    await ctx.send("La météo est bien !")
     
 bot.run(TOKEN)
 
+
+def get_weather(ville):
+    # Exemple avec l'API Open-Meteo (Coordonnées de Paris par défaut)
+    url = "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&current_weather=true"
+    response = requests.get(url)
+    data = response.json()
+    
+    temp = data['current_weather']['temperature']
+    return f"Il fait actuellement {temp}°C à Paris."
+
+print(get_weather("Paris"))
